e5dafc83fe1672de45ab1bc40e7f34876e2d1d15
kokke <spam@rowdy.dk>
2017-05-01 21:34:22 +0200

    Update test_rand.c

--- tests/test_rand.c
+++ tests/test_rand.c
@@ -1,3 +1,11 @@
+/*
+    This program tries to match a given regular expression with text given as input to stdin.
+    If the text is a match for the pattern, the program returns 0.
+    If the text doesn't match the pattern, the program returns -2.
+    
+    This program is used in random testing to test a lot of random text and regex together.
+    See ./scripts/regex_test.py and the Makefile for this project for the gritty details.
+*/
 
 #include <stdio.h>
 #include "re.h"

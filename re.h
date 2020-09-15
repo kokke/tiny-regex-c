0c94a3d3623692ea10a755bbd49406347693d468
ayangd <jimmyhendrixx90@gmail.com>
2020-09-15 20:52:51 +0700

    Fix typo on matchlenght to matchlength

--- re.h
+++ re.h
@@ -51,11 +51,11 @@ re_t re_compile(const char* pattern);
 
 
 /* Find matches of the compiled pattern inside text. */
-int  re_matchp(re_t pattern, const char* text, int* matchlenght);
+int  re_matchp(re_t pattern, const char* text, int* matchlength);
 
 
 /* Find matches of the txt pattern inside text (will compile automatically first). */
-int  re_match(const char* pattern, const char* text, int* matchlenght);
+int  re_match(const char* pattern, const char* text, int* matchlength);
 
 
 #ifdef __cplusplus
